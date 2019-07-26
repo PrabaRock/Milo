a4cef77a8e9f28fb1c9f4a54fa7edfd624f85013
PrabaRock <53283889+PrabaRock@users.noreply.github.com>
2019-07-26 14:45:45 +0530

    Create main.py

---
+++ cogs/main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Main(commands.Cog):
+	
+	def __init__(self, client):
+		self.client = client
+		
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print("Bot is Online :)")
+		
+	@commands.command()
+	async def ping(self, ctx):
+		await ctx.send(f"Pong {round(client.latenct * 1000)} ms")
+
+def setup(client):
+	client.add_cog(Main(client))
