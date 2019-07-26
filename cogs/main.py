b28d4b4c2230763a8706adfc439f57521bec0a8c
PrabaRock <53283889+PrabaRock@users.noreply.github.com>
2019-07-26 15:42:36 +0530

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -14,5 +14,23 @@ class Main(commands.Cog):
 	async def ping(self, ctx):
 		await ctx.send("Pong")
 
+        @commands.command()
+	async def myavatar(self, ctx):
+		show_avatar = discord.Embed(
+		
+		         color = discord.Color.dark_blue()
+		)
+		show_avatar.set_image(url="{}".format(ctx.author.avatar_url))
+		await ctx.send(embed=show_avatar)
+		
+	@commands.command()
+        async def useravatar(self, ctx, member : discord.Member):
+                show_avatar = discord.Embed(
+      
+                         color = discord.Color.blue()
+                )
+                show_avatar.set_image(URL="{}".format(member.avatar_url))
+                await ctx.send(embed=show_avatar)
+
 def setup(client):
 	client.add_cog(Main(client))
